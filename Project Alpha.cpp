1bb2afb871770f73febff78587d7f4cfabf61b7c
altont <71altont@gmail.com>
2017-02-12 23:26:01 -0800

    more stuff

--- Project Alpha.cpp
+++ Project Alpha.cpp
@@ -24,11 +24,11 @@ public:
 };
 
 void arm::init() {
-	
+	vector<int> arm;
 };
 
 void arm::pull() {
-	double mu;
+	/*double mu;
 	double sigma;
 	double generateGaussianNoise(double mu, double sigma);
 	{
@@ -51,9 +51,9 @@ void arm::pull() {
 
 		z0 = sqrt(-2.0 * log(u1)) * cos(two_pi * u2);
 		z1 = sqrt(-2.0 * log(u1)) * sin(two_pi * u2);
-		return z0 * sigma + mu;
+		return z0 * sigma + mu;*/
 	}
-}
+};
 
 class MAB {
 public:
@@ -63,6 +63,10 @@ public:
 
 void MAB::init() {
 	int n;
+	vector<int> MAB;
+	for (int i = 1; i < n; i++) {
+
+	}
 }
 
 class agent {
@@ -98,11 +102,36 @@ void agent::reset() {
 int main()
 {
 	srand(time(NULL));
-	double test;
-	for (int i = 1; i < 100; i++) {
-		test = ATRAND * 0.9;
-		cout << test << endl;
+	
+	double mu;
+	double sigma;
+	double generateGaussianNoise(double mu, double sigma);
+	{
+		const double epsilon = std::numeric_limits<double>::min();
+		const double two_pi = 2.0*3.14159265358979323846;
+
+		static double z0, z1;
+		static bool generate;
+		generate = !generate;
+
+		if (!generate)
+			return z1 * sigma + mu;
+
+		double u1, u2;
+		do
+		{
+			u1 = rand() * (1.0 / RAND_MAX);
+			u2 = rand() * (1.0 / RAND_MAX);
+		} while (u1 <= epsilon);
+
+		z0 = sqrt(-2.0 * log(u1)) * cos(two_pi * u2);
+		z1 = sqrt(-2.0 * log(u1)) * sin(two_pi * u2);
+		return z0 * sigma + mu;
 	}
+
+	vector<int> MAB;
+
+
     return 0;
 }
 
